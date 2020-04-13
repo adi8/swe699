85fa8ce1594c2a21138f48c630d0a9123b8c7580
Aditya Venkateshwaran <adiven01@gmail.com>
2020-04-13 13:51:00 -0400

    Added lombok dependency and updated order controller naming

--- composite-service/src/main/java/edu/gmu/swe699/composite/OrderController.java
+++ composite-service/src/main/java/edu/gmu/swe699/composite/OrderController.java
@@ -1,12 +1,16 @@
 package edu.gmu.swe699.composite;
 
 import edu.gmu.swe699.composite.services.CompositeServiceImpl;
+import edu.gmu.swe699.dynamodb.model.Order;
 import edu.gmu.swe699.dynamodb.model.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OrderLandingController {
@@ -23,5 +27,12 @@ public class OrderLandingController {
         Restaurant restaurant = implementation.getRestaurant(id);
         model.addAttribute("restaurant", restaurant);
         return "order-landing";
+    }
+
+    @PostMapping(value = "/order/review", consumes = "application/JSON", produces = "text/plain")
+    @ResponseBody
+    public String reviewOrder(@RequestBody Order order) {
+        System.out.println("Order: " + order.toString());
+        return "order-placed";
     }
 }
